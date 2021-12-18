badce8224cbd8a1d225682d7bc117bafe949b1d6
Robin Adair <robair1337@gmail.com>
2021-12-18 00:06:02 -0700

    Reinitalization with properly working folders

---
+++ RL-Stock-Trader-Ensemble-Method[2]/config/config.py
@@ -0,0 +1,33 @@
+import pathlib
+
+#import finrl
+
+import pandas as pd
+import datetime
+import os
+#pd.options.display.max_rows = 10
+#pd.options.display.max_columns = 10
+
+
+#PACKAGE_ROOT = pathlib.Path(finrl.__file__).resolve().parent
+#PACKAGE_ROOT = pathlib.Path().resolve().parent
+
+#TRAINED_MODEL_DIR = PACKAGE_ROOT / "trained_models"
+#DATASET_DIR = PACKAGE_ROOT / "data"
+
+# data
+#TRAINING_DATA_FILE = "data/ETF_SPY_2009_2020.csv"
+TRAINING_DATA_FILE = "data/dow_30_2009_2020.csv"
+
+now = datetime.datetime.now()
+#TRAINED_MODEL_DIR = f"trained_models/{now}"
+#TRAINED_MODEL_DIR = f"trained_models/TestRun" # 1/100th size zone
+#TRAINED_MODEL_DIR = f"trained_models/2xTestRun" # / 2x size zone. 
+TRAINED_MODEL_DIR = f"trained_models/100thRun2" # / 1/100th size zone. 
+
+os.makedirs(TRAINED_MODEL_DIR)
+TURBULENCE_DATA = "data/dow30_turbulence_index.csv"
+
+TESTING_DATA_FILE = "test.csv"
+
+
